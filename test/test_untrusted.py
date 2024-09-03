48b0ad3d1c56075c0062b964e0f73442b113dbfe
Michael Biebl <biebl@debian.org>
2024-09-03 14:46:13 +0200

    unattended-upgrades (2.11+nmu1) unstable; urgency=medium
      * Non-maintainer upload.
      * Install systemd-sleep part into /usr/lib. (Closes: #1073745)
    
    [dgit import package unattended-upgrades 2.11+nmu1]

---
+++ test/test_untrusted.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import os
+import unittest
+
+from test.test_base import TestBase, MockOptions
+
+import unattended_upgrade
+
+
+class TestUntrusted(TestBase):
+
+    def setUp(self):
+        TestBase.setUp(self)
+        self.rootdir = self.make_fake_aptroot(
+            template=os.path.join(self.testdir, "root.untrusted"))
+        self.log = os.path.join(
+            self.rootdir, "var", "log", "unattended-upgrades",
+            "unattended-upgrades.log")
+
+    def test_untrusted_check_without_conffile_check(self):
+        options = MockOptions()
+        unattended_upgrade.main(options, rootdir=self.rootdir)
+        # read the log to see what happend
+        with open(self.log) as f:
+            haystack = f.read()
+            self.assertTrue(
+                "pkg test-package is not from a trusted origin" in haystack)
+            if unattended_upgrade.get_distro_codename() == "sid":
+                self.assertFalse(
+                    "falling back to adjusting" in haystack)
+            else:
+                self.assertTrue(
+                    "falling back to adjusting" in haystack)
+
+
+if __name__ == "__main__":
+    # do not setup logging in here or the test will break
+    unittest.main()
